70dc09ae1abfb2c962c6af95670bdfce106c2bcc
rezasohbati <r.sohbati@bluteam.ir>
2024-01-10 16:44:14 +0330

    resolve test running problem in branch folder

--- branch-teller/Branch/src/test/java/ir/rahgozin/branch/BranchApplicationTests.java
+++ branch-teller/Branch/src/test/java/ir/rahgozin/branch/BranchApplicationTests.java
@@ -1,13 +1,21 @@
 package ir.rahgozin.branch;
 
+import ir.rahgozin.branch.config.TestConfig;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-class BranchApplicationTests {
+@ContextConfiguration(classes = TestConfig.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")class BranchApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
